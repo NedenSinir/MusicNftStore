99cf1d6dc36e39143de798a91d1fb4f594a66e08
NedenSinir <84202527+NedenSinir@users.noreply.github.com>
2022-04-17 00:15:12 +0300

    Delete escrow.js

--- app/src/escrow.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
-import { Program, Provider, web3 } from '@project-serum/anchor';
-import idl from './idl.json';
-import kp from './keypair.json'
